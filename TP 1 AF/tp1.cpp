69e60c252fcfc9b99f13b68399db0122e3b28712
DamianMarzocca <dmarzocca@frba.utn.edu.ar>
2024-05-02 19:25:39 -0300

    Primer commit no funca

--- TP 1 AF/tp1.cpp
+++ TP 1 AF/tp1.cpp
@@ -1,8 +1,64 @@
-#include <stdio.h>
+#include <stdio.h> // printf /
+#include <ctype.h> // isdigit /
 
 
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+void Titulo (void);   // funcion auxiliar /
+int Verifica (char);  // que los caracteres pertenezcan al alfabeto /
+int Columna (int);  //dado un caracter, determina la columna  /
+int EsPalabra (const char);
+
+int main ()
+{
+    char s1[] = "14-5";
+    Titulo();
+    if (! Verifica(s1))
+        printf("La cadena s1 tiene caracteres invalidos\n");
+    else
+        if (EsPalabra(s1))
+            printf("La cadena s1 pertenece al lenguaje\n");
+        else
+            printf("La cadena s1 no pertenece al lenguaje\n");
+    return 0;
+} //* fin main /
+
+void Titulo (void)
+{
+    printf("Este programa prueba un AFD que determina\n");
+    printf("si una cadena dada corresponde a un numero entero\n\n");
+} // fin Titulo /
+
+int Verifica (chars)
+{
+    unsigned i;
+    for (i=0; s[i] != '\0'; i++)
+        if (isletter)
+            return 1;
+    return 0;
+} //* fin Verifica /
+
+int Columna (int c)
+{
+    switch (c)
+    {
+        case '+':
+            return 1;
+        case '-':
+            return 2;
+        default / es digito /:
+            return 0;
+    }
+} // fin Columna /
+
+int EsPalabra (const charcadena)
+{
+    static int tt [4][4] =
+                {{1,3,3,3}, //* Tabla de Transiciones /
+                {1,2,2,3}, // 2 es el estado final /
+                {1,3,3,3}, // 3 es el estado de rechazo /
+                {3,3,3,3}};
+    int e; // estado actual del automata /
+    unsigned int i; // recorre la cadena /
+    for (e=0,i=0; cadena[i]!='\0' && e!=3; i++)
+        e = tt [e][Columna(cadena[i])];
+    return e==2;    // estado final? retorna 1 /
+}  //fin EsPalabra2 */
